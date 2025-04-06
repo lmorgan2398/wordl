6e52fc11fe9c65a070e7b3e44db32292953ccafe
lmorgan2398 <lmorgan2398@gmail.com>
2025-04-05 23:06:34 -0500

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
