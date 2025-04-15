2257f117b22b145125469cc180613d21d90a3d32
lmorgan2398 <lmorgan2398@gmail.com>
2025-04-14 19:51:28 -0500

    Update NPM scripts and split config files

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        watchFiles: ['./src/template.html'],
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
